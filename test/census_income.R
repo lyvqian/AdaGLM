f35194eb60d0912fdd0ea301dcdd4634eab22d83
lyvqian <leyuan.qian@outlook.com>
2025-04-07 15:29:08 -0400

    Update Data Analysis

--- test/census_income.R
+++ test/census_income.R
@@ -4,21 +4,23 @@ library(dplyr)
 income = read.csv("census_income.csv")
 
 X <- model.matrix(
-  ~ marital_group + log_age + education_level + race_two + sex + occupation_group + native_us,
+  ~ age + sex + education_level,
   data = income
 )
 y <- income$income_two
 
-model <- glm.fit(x = X, y = y, family = binomial())
+model <- glm(income_two ~ age + sex + education_level, data = income[1:500, ], family = binomial())
 beta_glm <- coef(model) # does not converge, coefficients almost 0 except the intercept 
 
-X_inter <- cbind(rep(1,nrow(X)), X)
+#X_inter <- cbind(rep(1,nrow(X)), X[,2])
+#X_inter <- cbind(rep(1,500), X[1:500,2])
+X_inter <- X[1:500,]
 family = "binomial_logit"
 
-beta_adam <- adaglm(X_inter,y,fam_link = family, optimizer = "ADAM")
-beta_adagrad <- adaglm(X_inter,y,fam_link = family, optimizer = "AdaGrad")
-beta_adadelta <- adaglm(X_inter,y,fam_link = family, optimizer = "AdaDelta")
-beta_adasmooth <- adaglm(X_inter,y,fam_link = family, optimizer = "AdaSmooth")
+beta_adam <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "ADAM")
+beta_adagrad <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "AdaGrad", alpha=0.1)
+beta_adadelta <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "AdaDelta")
+beta_adasmooth <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "AdaSmooth")
 
 beta_mat <- cbind(beta_adam$coefficients, beta_adagrad$coefficients, beta_adadelta$coefficients, beta_adasmooth$coefficients, as.numeric(beta_glm))
 colnames(beta_mat) = c("ADAM", "AdaGrad", "AdaDelta", "AdaSmooth", "glm_fn")
