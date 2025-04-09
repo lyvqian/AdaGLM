a4f0b47db1f5e393f016eb18bf1dc5e0def4c826
lyvqian <leyuan.qian@outlook.com>
2025-04-09 16:02:47 -0400

    Update Name

--- test/census_income.R
+++ test/census_income.R
@@ -17,10 +17,10 @@ beta_glm <- coef(model) # does not converge, coefficients almost 0 except the in
 X_inter <- X[1:500,]
 family = "binomial_logit"
 
-beta_adam <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "ADAM")
+beta_adam <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "ADAM",alpha=0.1)
 beta_adagrad <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "AdaGrad", alpha=0.1)
-beta_adadelta <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "AdaDelta")
-beta_adasmooth <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "AdaSmooth")
+beta_adadelta <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "AdaDelta", rho=0.99)
+beta_adasmooth <- adaglm(X_inter,y[1:500],fam_link = family, optimizer = "AdaSmooth", alpha=0.1)
 
 beta_mat <- cbind(beta_adam$coefficients, beta_adagrad$coefficients, beta_adadelta$coefficients, beta_adasmooth$coefficients, as.numeric(beta_glm))
 colnames(beta_mat) = c("ADAM", "AdaGrad", "AdaDelta", "AdaSmooth", "glm_fn")
